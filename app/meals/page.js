991828c457b1c66e07f6ab0c2bf05238a43dd80e
vjiji <restmi0711@gmail.com>
2024-05-27 23:08:50 +0900

    메인 리스트 컴포넌트 생성 및 스타일 작업

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,25 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/MealsGrid";
+
 export default function MealsPage() {
-  return <h1>meals page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favirute recipe and cook it yourself. If is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
